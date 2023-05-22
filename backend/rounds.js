70da3aa186ed6ae501633a6c92df66834be184a5
AlexanderFasching <alexander.fasching@stud.fh-campuswien.ac.at>
2023-05-22 15:30:51 +0200

    playable game with 5 rounds

---
+++ backend/rounds.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id1: "946422635936572",
+        id2: "2692168084406521",
+        correct: 1,
+        question: "Which image is Austria!"
+    },
+    {
+        id1: "477961909951886",
+        id2: "1232139864226680",
+        correct: 1,
+        question: "Which image is closer to you?"
+    },
+    {
+        id1: "173684327997946",
+        id2: "344148887106394",
+        correct: 2,
+        question: "Which image is your favoutite university(FH-Campus)?"
+    },
+    {
+        id1: "477961909951886",
+        id2: "1232139864226680",
+        correct: 1,
+        question: "Where are you more likely to get "
+    },
+    {
+        id1: "477961909951886",
+        id2: "1232139864226680",
+        correct: 1,
+        question: "Which is Yo Mama?"
+    }
+]
