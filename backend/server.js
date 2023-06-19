2729d62976a47b962db070560e54dfb1cd7e6f26
MBGamesStudios <azeigs-github@yahoo.com>
2023-06-19 23:57:40 +0200

    tried adding basic session management changed webpack bundler target in config added .env file

--- backend/server.js
+++ backend/server.js
@@ -6,22 +6,84 @@ const rounds = require("./rounds.js");
 const app = express();
 
 // Parse urlencoded bodies
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, '..', 'src')));
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 app.get("/StreetViewsPls", function (req, res) {
-  let round = Object.values(rounds);
-  console.log(round[req.query.round - 1]);
-  res.send(round[req.query.round - 1]);
+    let round = Object.values(rounds);
+    console.log(round[req.query.round - 1]);
+    res.send(round[req.query.round - 1]);
 })
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'src', 'main.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'src', 'main.html'));
+});
+
+
+// session management
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
+app.use(session({
+        secret: process.env.SESSION_SECRET || "blablub",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+let user = {};
+const users = [user];
+
+app.post("/login", (req, res) => {
+    user.name = req.query.name;
+    user.password = req.query.password;
+    user.highscore = 0;
+
+    let userExists = false;
+    for (const u of users) {
+        if (u.name === user.name) {
+            userExists = true;
+            if (u.password === user.password) {
+                user = u;
+            } else return res.status(200).json({info: `[${user.name}]: invalid password`})
+        }
+    }
+    if (!userExists) users.push(user);
+    console.log("Users:");
+    console.log(users);
+
+    req.session.user = user;
+    req.session.save();
+    return res.status(201).json({info: `[${user.name}] logged in`});
+});
+
+app.get("/users", (req, res) => {
+    return res.status(200).json(users);
+});
+
+app.get("/user", (req, res) => {
+    if (req.session.user != null) {
+        return res.status(200)
+            .json({info: `[${user.name}] profile info`, data: req.session.user});
+    } else return res.status(200).json({info: "No data present in session"});
+});
+
+app.post("/logout", (req, res) => {
+    req.session.destroy();
+    return res.status(204).json({info: `[${user.name}] logged out`});
+});
+
+app.delete("/deregister", (req, res) => {
+
 });
 
 app.listen(3000)
-
 console.log("Server now listening on http://localhost:3000/")
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /*
   Mapillary Stuff
 */
-
 import {Viewer} from 'mapillary-js';
 
 let round = 1;
@@ -24,7 +23,7 @@ function getViews(round) {
                 imageId: r['id1'],
             });
             viewer1.deactivateCover();
-
+            console.log(r);
             viewer2 = new Viewer({
                 accessToken: 'MLY|6425749720781602|74d4571106775c1ff773082d77b80f27',
                 container: 'mly2',
@@ -35,8 +34,9 @@ function getViews(round) {
             const questionElement = document.getElementById('question');
             questionElement.innerText = r['question']
             correctAnswer = r['correct'];
-            if(timerMode === true){
-            startCountdown();}
+            if (timerMode === true) {
+                startCountdown();
+            }
             console.log("Round: " + round);
             console.log("Points: " + score);
 
@@ -52,43 +52,43 @@ function getViews(round) {
 }
 
 function startCountdown() {
-  let timeleft = 30;
-  const countdownElement = document.getElementById('timer-id');
-  countdownElement.innerHTML = timeleft + ' seconds remaining';
-  countdownInterval = setInterval(function () {
-    if (timeleft <= 0) {
-      countdownElement.innerHTML = "Time's up!";
-      endRound(null);
-    } else {
-      timeleft -= 1;
-      countdownElement.innerHTML = timeleft + ' seconds remaining';
-    }
-  }, 1000);
+    let timeleft = 30;
+    const countdownElement = document.getElementById('timer-id');
+    countdownElement.innerHTML = timeleft + ' seconds remaining';
+    countdownInterval = setInterval(function () {
+        if (timeleft <= 0) {
+            countdownElement.innerHTML = "Time's up!";
+            endRound(null);
+        } else {
+            timeleft -= 1;
+            countdownElement.innerHTML = timeleft + ' seconds remaining';
+        }
+    }, 1000);
 }
 
 function endRound(answer) {
-  clearInterval(countdownInterval); // clear countdown interval
+    clearInterval(countdownInterval); // clear countdown interval
 
-  if (answer === correctAnswer) {
-    score += 100;
-    updateScore();
-    
-  }
-  console.log('Round: ' + round);
-  console.log('Score: ' + score);
+    if (answer === correctAnswer) {
+        score += 100;
+        updateScore();
 
-  if (round >= 5) {
-    const roundElement = document.getElementById('round-id');
-    roundElement.textContent = "End of Game";
-    clearInterval(countdownInterval);
-    timerElement.textContent = "Time is up!";
-    console.log('End of Game');
-    console.log('Final Score: ' + score + ' Points');
-  } else {
-    round++;
-    updateRound();
-    getViews(round);
-  }
+    }
+    console.log('Round: ' + round);
+    console.log('Score: ' + score);
+
+    if (round >= 5) {
+        const roundElement = document.getElementById('round-id');
+        roundElement.textContent = "End of Game";
+        clearInterval(countdownInterval);
+        timerElement.textContent = "Time is up!";
+        console.log('End of Game');
+        console.log('Final Score: ' + score + ' Points');
+    } else {
+        round++;
+        updateRound();
+        getViews(round);
+    }
 }
 
 
@@ -111,64 +111,64 @@ document.getElementById("nav-theme").addEventListener("click", toggleTheme);
 
 document.addEventListener("DOMContentLoaded", () => {
     const themeToggleBtn = document.getElementById("nav-theme");
-  
+
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-  
+
     const setTheme = (prefersDark) => {
-      const body = document.querySelector("body");
-  
-      if (prefersDark) {
-        body.classList.remove("light-theme");
-        body.classList.add("dark-theme");
-        themeToggleBtn.innerHTML =
-          '<span class="material-icons">nights_stay</span>';
-        themeToggleBtn.classList.remove("light-theme-icon");
-        themeToggleBtn.classList.add("dark-theme-icon");
-      } else {
-        body.classList.remove("dark-theme");
-        body.classList.add("light-theme");
-        themeToggleBtn.innerHTML = '<span class="material-icons">wb_sunny</span>';
-        themeToggleBtn.classList.remove("dark-theme-icon");
-        themeToggleBtn.classList.add("light-theme-icon");
-      }
+        const body = document.querySelector("body");
+
+        if (prefersDark) {
+            body.classList.remove("light-theme");
+            body.classList.add("dark-theme");
+            themeToggleBtn.innerHTML =
+                '<span class="material-icons">nights_stay</span>';
+            themeToggleBtn.classList.remove("light-theme-icon");
+            themeToggleBtn.classList.add("dark-theme-icon");
+        } else {
+            body.classList.remove("dark-theme");
+            body.classList.add("light-theme");
+            themeToggleBtn.innerHTML = '<span class="material-icons">wb_sunny</span>';
+            themeToggleBtn.classList.remove("dark-theme-icon");
+            themeToggleBtn.classList.add("light-theme-icon");
+        }
     };
-  
+
     setTheme(prefersDarkScheme.matches);
-  
+
     themeToggleBtn.addEventListener("click", () => {
-      // Wenn das aktuelle Theme dunkel ist, setzen Sie es auf hell und umgekehrt
-      const body = document.querySelector("body");
-      const currentIsDark = body.classList.contains("dark-theme");
-      setTheme(!currentIsDark);
+        // Wenn das aktuelle Theme dunkel ist, setzen Sie es auf hell und umgekehrt
+        const body = document.querySelector("body");
+        const currentIsDark = body.classList.contains("dark-theme");
+        setTheme(!currentIsDark);
     });
-  });
-  
-  //++++++++++++++++++++++++++++
-  
-  const showSection = (sectionId) => {
+});
+
+//++++++++++++++++++++++++++++
+
+const showSection = (sectionId) => {
     // Hide all sections
     document.querySelectorAll("main > section").forEach((section) => {
-      section.classList.add("hidden");
-     
+        section.classList.add("hidden");
+
     });
-  
+
     // Show the selected section
     document.getElementById(sectionId).classList.remove("hidden");
-  };
+};
 
-  document.getElementById("nav-home").addEventListener("click", () => {
+document.getElementById("nav-home").addEventListener("click", () => {
     showSection("home-view");
-  });
-  
-  document.getElementById("nav-leaderboard").addEventListener("click", () => {
+});
+
+document.getElementById("nav-leaderboard").addEventListener("click", () => {
     showSection("leaderboard-view");
-  });
-  
-  document.getElementById("nav-login").addEventListener("click", () => {
+});
+
+document.getElementById("nav-login").addEventListener("click", () => {
     showSection("login-view");
-  });
-  
-  document.getElementById("quick-game").addEventListener("click", () => {
+});
+
+document.getElementById("quick-game").addEventListener("click", () => {
     timerMode = false;
     round = 1;
     updateRound();
@@ -176,62 +176,64 @@ document.addEventListener("DOMContentLoaded", () => {
     updateScore();
     getViews(1);
     showSection("game-view");
-    if(timerElement) {
-      timerElement.parentNode.removeChild(timerElement);
-      timerElement = null;
+    if (timerElement) {
+        timerElement.parentNode.removeChild(timerElement);
+        timerElement = null;
     }
-  });
+});
 
-  let timerElement;
-  document.getElementById("timer-game").addEventListener("click", () => {
+let timerElement;
+document.getElementById("timer-game").addEventListener("click", () => {
     round = 1;
     updateRound();
     score = 0;
     updateScore();
     timerMode = true;
     clearInterval(countdownInterval);
-    getViews(1); 
+    getViews(1);
     showSection("game-view");
-    if(!timerElement) {
-    timerElement = document.createElement("p");
-    timerElement.setAttribute("id", "timer-id");
-    let containerScore = document.getElementById("score");
-    containerScore.appendChild(timerElement);
-    timerElement.append("Countdown: ");
+    if (!timerElement) {
+        timerElement = document.createElement("p");
+        timerElement.setAttribute("id", "timer-id");
+        let containerScore = document.getElementById("score");
+        containerScore.appendChild(timerElement);
+        timerElement.append("Countdown: ");
     }
-  });
-  
-  document.getElementById("home-login").addEventListener("click", () => {
-    showSection("login-view");
-  });
+});
 
-  // Listener for game buttons
+document.getElementById("home-login").addEventListener("click", () => {
+    showSection("login-view");
+});
+
+// Listener for game buttons
 document.getElementById("b1").addEventListener("click", () => {
     endRound(1);
 })
 document.getElementById("b2").addEventListener("click", () => {
     endRound(2);
 })
-  
-  // Show the home view by default
-  showSection("home-view");
-  
-  
-  function updateScore() {
-    scoreElement.textContent = "Score: "+score;
-  }
-  function updateRound() {
-    roundElement.textContent = "Round: "+round;
-  }
-  let roundElement = document.createElement("p");
-  roundElement.setAttribute("id", "round-id");
-  let containerScore = document.getElementById("score");
-  containerScore.appendChild(roundElement);
-  
-  let scoreElement = document.createElement("p");
-  scoreElement.setAttribute("id", "score-id")
-  containerScore.appendChild(scoreElement);
-  updateScore();
-  updateRound();
+
+// Show the home view by default
+showSection("home-view");
+
+
+function updateScore() {
+    scoreElement.textContent = "Score: " + score;
+}
+
+function updateRound() {
+    roundElement.textContent = "Round: " + round;
+}
+
+let roundElement = document.createElement("p");
+roundElement.setAttribute("id", "round-id");
+let containerScore = document.getElementById("score");
+containerScore.appendChild(roundElement);
+
+let scoreElement = document.createElement("p");
+scoreElement.setAttribute("id", "score-id")
+containerScore.appendChild(scoreElement);
+updateScore();
+updateRound();
 
 
