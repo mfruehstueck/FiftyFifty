c8025d8d5dfd67e3d844dd99fa98e082220601eb
MBGamesStudios <azeigs-github@yahoo.com>
2023-06-26 22:12:11 +0200

    fixed some login issues moved logic to server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -5,18 +9,87 @@ const rounds = require("./rounds.js");
 
 const app = express();
 
-// Parse urlencoded bodies
-app.use(bodyParser.json());
+const sessions = require("express-session");
+const cookieParser = require("cookie-parser");
 
+// Parse urlencoded bodies
+// app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, '..', 'src')));
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+//session
+app.use(sessions({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: true,
+        cookie: { maxAge: parseMathExpression(process.env.SESSION_MAXAGE) },
+        resave: false
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.use(cookieParser());
 
 app.get("/StreetViewsPls", function (req, res) {
-    let round = Object.values(rounds);
-    console.log(round[req.query.round - 1]);
-    res.send(round[req.query.round - 1]);
+    // game rounds
+    // -1 ... startOfGame
+    // 0 ... infiniteRounds
+    // x ... roundCount
+
+    console.log(":::::::StreetViewsPls:::::::");
+    console.log(req.session.username);
+
+    const tmpUser = getUser(req.session.username);
+    console.log(tmpUser);
+    if (tmpUser === null) {
+        console.log("NOPE");
+
+        res.redirect("/");
+        return;
+    }
+
+    let currentRound = {
+        roundIdx: 0,
+        question: rounds[0].question,
+        imageID1: rounds[0].id1,
+        imageID2: rounds[0].id2,
+        quessWasCorrect: false,
+        currentScore: 0,
+        gameWon: false
+    };
+
+    if (tmpUser.game.round < 0 || req.header("roundIdx") < 0) {
+        console.log("START");
+        tmpUser.game.round = 0;
+        tmpUser.game.score = 0;
+
+    } else if (tmpUser.game.round >= tmpUser.game.rounds) {
+        console.log("WON");
+
+        if (tmpUser.game.score > tmpUser.highscore) tmpUser.highscore = tmpUser.game.score;
+        tmpUser.game.round = -1;
+
+        currentRound.currentScore = tmpUser.game.score;
+        currentRound.gameWon = true;
+        res.redirect("/");
+    } else {
+        console.log("NEXT");
+        if (req.headers.guess == rounds[tmpUser.game.round].correct) {
+            tmpUser.game.score += 100;
+            currentRound.quessWasCorrect = true;
+        }
+        currentRound.roundIdx = ++tmpUser.game.round;
+        currentRound.question = rounds[tmpUser.game.round].question;
+        currentRound.imageID1 = rounds[tmpUser.game.round].id1;
+        currentRound.imageID2 = rounds[tmpUser.game.round].id2;
+        currentRound.currentScore = tmpUser.game.score;
+
+    }
+    console.log("currentRound");
+    console.log(currentRound);
+    console.log("score " + tmpUser.score);
+    res.send(currentRound);
 })
 
 // no idea why this is needed but it does not work without it
@@ -26,51 +99,59 @@ app.get("/WTF", (req, res) => {
 
 // session management
 let session = {};
-let sessionList = [session];
 let user = {
     username: "",
     password: "",
     countryCode: "",
     highscore: 0,
     style: "dark",
-    lastTenPlayedGames: []
+    lastTenPlayedGames: [],
+
+    game: {
+        rounds: 4,
+        round: -1,
+        score: 0
+    }
 };
-let userList = [user];
-
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-
-const sessions = require("express-session");
-const cookieParser = require("cookie-parser");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+let userList = [];
 
 function parseMathExpression(input) {
     return Function(`'use strict'; return (${input})`)();
 }
 
 function getUser(username) {
+    // getUser if exists
+    if (username !== "") {
+        for (const u of userList) if (u.username === username) return u;
+    }
 
+    return null;
 }
 
-app.use(cookieParser());
-app.use(sessions({
-        secret: process.env.SESSION_SECRET,
-        saveUninitialized: true,
-        cookie: { maxAge: parseMathExpression(process.env.SESSION_MAXAGE) },
-        resave: false
-    })
-);
+function validateUser(username, password, countryCode) {
+    // returns user if found and adds to list if not
+    let tmpUser = getUser(username);
+    if (tmpUser !== null) {
+        if (tmpUser.password !== password) return null;
+    } else {
+        console.log(`[${username}] does not exist`);
+
+        tmpUser = { ...user };
+        console.log(tmpUser)
+        tmpUser.username = username;
+        tmpUser.password = password;
+        tmpUser.countryCode = countryCode;
+
+        userList.push(tmpUser);
+    }
+
+    return tmpUser;
+}
 
 app.get('/', function (req, res) {
     session = req.session;
-    console.log(session);
-    // if(session.userid){
+    console.log("user: " + session.username);
     res.sendFile(path.join(__dirname, '..', 'src', 'main.html'));
-    // } else console.log("Fuck");
 });
 
 app.get("/countryCodes", (req, res) => {
@@ -82,45 +163,26 @@ app.get("/countryCodes", (req, res) => {
 });
 
 app.post("/user", (req, res) => {
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.countryCode = req.body.countryCode;
+    // inputs from login-form
+    // let tmpUser = { user };
+    let username = req.header("username");
+    let password = req.header("password");
+    let countryCode = req.header("countryCode");
 
-    // console.log("User:");
-    // console.log(user);
-    let userExists = false;
-    for (const u of userList) {
-        if (u.username === user.username) {
-            userExists = true;
-            if (u.password === user.password) {
-                user = u;
-            } else return res.status(200).json({ info: `[${user.username}]: invalid password` })
-        }
+    user = validateUser(username, password, countryCode);
+
+    if (user !== null) {
+        session = req.session;
+        session.username = user.username;
+
+        res.redirect("/");
+    } else {
+        console.log(`[${username}] invalid password`);
     }
-    if (!userExists) userList.push(user);
-    else {
-        // req.session.username = user.username;
-    }
-    console.log("Users:");
-    console.log(userList);
-
-    session = req.session;
-    session.username = user.username;
-    session.countryCode = user.countryCode;
-    session.style = user.style;
-
-    res.redirect("/");
 });
 
 app.get("/users", (req, res) => {
     return res.status(200).json(userList);
-});
-
-app.post("/user", (req, res) => {
-    if (req.session.user != null) {
-        return res.status(200)
-            .json({ info: `[${user.name}] profile info`, data: req.session.user });
-    } else return res.status(200).json({ info: "No data present in session" });
 });
 
 app.post("/logout", (req, res) => {
@@ -133,9 +195,25 @@ app.delete("/deregister", (req, res) => {
 });
 
 // leaderboard
-let leaderboardList;
+const leaderboardEntry = {
+    rank: 0,
+    username: "",
+    score: 0
+}
+let leaderboardList = [];
+
+function updateLeaderboard(user) {
+    let tmpUser = leaderboardList.find((username) => username === user.username);
+    if (tmpUser) {
+        tmpUser.highscore = user.highscore;
+    } else {
+        leaderboardList.push(user);
+    }
+    leaderboardList.sort((a, b) => a.highscore - b.highscore);
+}
 
 app.get("/leaderboard", (req, res) => {
+    res.send(leaderboardList);
     console.log("leaderboard:");
 });
 
--- src/index.js
+++ src/index.js
@@ -12,31 +12,28 @@ let viewer1;
 let viewer2;
 
 let currentRound = {
-    roundIdx: 1,
-    imageID1: "946422635936572",
-    imageID2: "2692168084406521"
+    roundIdx: 0,
+    question: "",
+    imageID1: "",
+    imageID2: "",
+    currentScore: 0
 }
 
 
 function getViews(round, containerID1 = "mly1", containerID2 = "mly2") {
-    let imageID1 = "946422635936572";
-    let imageID2 = "2692168084406521";
-
     const xhr = new XMLHttpRequest();
     xhr.onload = () => {
-        console.log(imageID1);
-        console.log(containerID1);
         disposeViewers();
         viewer1 = new Viewer({
             accessToken: "MLY|6425749720781602|74d4571106775c1ff773082d77b80f27",
             container: containerID1,
-            imageId: imageID1,
+            imageId: round.imageID1,
         });
         viewer1.deactivateCover();
         viewer2 = new Viewer({
             accessToken: "MLY|6425749720781602|74d4571106775c1ff773082d77b80f27",
             container: containerID2,
-            imageId: imageID2,
+            imageId: round.imageID2,
         });
         viewer2.deactivateCover();
     }
@@ -129,26 +126,58 @@ function startCountdown() {
 function endRound(answer) {
     clearInterval(countdownInterval); // clear countdown interval
 
-    if (answer === correctAnswer) {
-        score += 100;
+    let xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        let tmpRound = JSON.parse(xhr.response);
+
+        currentRound.roundIdx = tmpRound.roundIdx;
+        currentRound.question = tmpRound.question;
+        currentRound.imageID1 = tmpRound.imageID1;
+        currentRound.imageID2 = tmpRound.imageID2;
+        currentRound.currentScore = tmpRound.currentScore;
+
+        console.log("currentRound answer: " + answer);
+        console.log(currentRound);
+
+        updateRound();
         updateScore();
 
-    }
-    console.log('Round: ' + currentRound.roundIdx);
-    console.log('Score: ' + score);
+        if (tmpRound.gameWon !== true) getViews(currentRound);
+        else {
+            const roundElement = document.getElementById('round-id');
+            roundElement.textContent = "End of Game";
+            if (timerMode) {
+                clearInterval(countdownInterval);
+                timerElement.textContent = "Time is up!";
+            }
+            console.log('End of Game');
+            console.log('Final Score: ' + currentRound.currentScore + ' Points');
+            showSection("home-view");
+        }
+    };
+    xhr.open("GET", "/StreetViewsPls");
+    xhr.setRequestHeader("guess", answer);
+    xhr.send();
 
-    if (currentRound.roundIdx >= 5) {
-        const roundElement = document.getElementById('round-id');
-        roundElement.textContent = "End of Game";
-        clearInterval(countdownInterval);
-        timerElement.textContent = "Time is up!";
-        console.log('End of Game');
-        console.log('Final Score: ' + score + ' Points');
-    } else {
-        currentRound.roundIdx++;
-        updateRound();
-        getViews(currentRound);
-    }
+    // if (answer === correctAnswer) {
+    //     score += 100;
+    //     updateScore();
+    //
+    // }
+    //
+    //
+    // if (currentRound.roundIdx >= 5) {
+    //     const roundElement = document.getElementById('round-id');
+    //     roundElement.textContent = "End of Game";
+    //     clearInterval(countdownInterval);
+    //     timerElement.textContent = "Time is up!";
+    //     console.log('End of Game');
+    //     console.log('Final Score: ' + currentRound.currentScore + ' Points');
+    // } else {
+    //     currentRound.roundIdx++;
+    //     updateRound();
+    //     getViews(currentRound);
+    // }
 }
 
 
@@ -202,7 +231,6 @@ const showSection = (sectionId) => {
     // Hide all sections
     document.querySelectorAll("main > section").forEach((section) => {
         section.classList.add("hidden");
-
     });
 
     // Show the selected section
@@ -218,26 +246,49 @@ document.getElementById("nav-leaderboard").addEventListener("click", () => {
 });
 
 
-document.getElementById("login-form").addEventListener("submit", () => {
-    alert("asdf");
-    fetch("/user")
-        .then(() => showSection("leaderboard-view"))
-        .catch(error => console.log(error));
-});
-document.getElementById("nav-login").addEventListener("click", () => {
-    showSection("login-view");
+document.getElementById("login-form").addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const input_username = document.getElementById("username");
+    const input_password = document.getElementById("password");
+    const sel_countryCode = document.getElementById("countryCode");
+
+    if (input_username.value === "" || input_password.value === "") alert("Ensure you input a value in both fields!");
+    else {
+        alert("Welcome [" + input_username.value + "]");
+
+        const xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+            document.getElementById("login-form").reset();
+            clearCountryCodes(sel_countryCode);
+
+            showSection("leaderboard-view");
+        };
+        xhr.open("POST", "/user");
+        xhr.setRequestHeader("username", input_username.value);
+        xhr.setRequestHeader("password", input_password.value);
+        xhr.setRequestHeader("countryCode", sel_countryCode.value);
+        xhr.send();
+    }
+});
+
+function clearCountryCodes(sel_countryCode) {
+    for (let i = 0; i <= sel_countryCode.options.length; i++) {
+        sel_countryCode.options[i] = null;
+    }
+}
+
+document.getElementById("nav-login").addEventListener("click", () => {
     const sel_countryCode = document.getElementById("countryCode");
     let sel_countryCode_option;
     let sel_countryCode_option_text;
 
 
-
     fetch("/countryCodes")
         .then(response => response.json())
         .then(data => {
+            clearCountryCodes(sel_countryCode);
             for (const countryCodeValue of data) {
-                console.log(countryCodeValue);
                 sel_countryCode_option = document.createElement("option");
                 sel_countryCode_option.setAttribute("value", countryCodeValue);
 
@@ -249,16 +300,32 @@ document.getElementById("nav-login").addEventListener("click", () => {
         })
         .catch(error => console.log(error));
 
-
+    showSection("login-view");
 });
 
 function initGame() {
-    currentRound.roundIdx = 1;
-    score = 0;
-    updateRound();
-    updateScore();
-    getViews(currentRound);
-    showSection("game-view");
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        let tmpRound = JSON.parse(xhr.response);
+
+        currentRound.roundIdx = tmpRound.roundIdx;
+        currentRound.question = tmpRound.question;
+        currentRound.imageID1 = tmpRound.imageID1;
+        currentRound.imageID2 = tmpRound.imageID2;
+        currentRound.currentScore = tmpRound.currentScore;
+
+        console.log(currentRound);
+
+        updateRound();
+        updateScore();
+
+        getViews(currentRound);
+
+        showSection("game-view");
+    }
+    xhr.open("GET", "/StreetViewsPls");
+    xhr.setRequestHeader("roundIdx", "-1");
+    xhr.send();
 }
 
 document.getElementById("quick-game").addEventListener("click", () => {
@@ -295,16 +362,20 @@ document.getElementById("b2").addEventListener("click", () => {
     endRound(2);
 })
 
+function playRound(answer) {
+
+}
+
 // Show the home view by default
 showSection("home-view");
 
 
 function updateScore() {
-    scoreElement.textContent = "Score: " + score;
+    scoreElement.textContent = "Score: " + currentRound.currentScore;
 }
 
 function updateRound() {
-    roundElement.textContent = "Round: " + currentRound.roundIdx;
+    roundElement.textContent = "Round: " + (currentRound.roundIdx + 1);
 }
 
 let roundElement = document.createElement("p");
